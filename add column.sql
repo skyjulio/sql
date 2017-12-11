a6e1313e4600f1a1f0ab04a7431de06f2f7f7e04
skyjulio <skyjulio@gmail.com>
2017-12-11 12:03:22 -0200

    add column
    ok

---
+++ add column.sql
@@ -0,0 +1,84 @@
+use OCORRENCIA 
+
+ALTER TABLE TR_TEMPO_RECURSO
+  ADD DT_ULTIMA_ATUALIZACAO datetime NULL 
+
+
+  	text
+
+ALTER TABLE AspNetUsers
+  ADD ULTIMO_ACESSO datetime NULL 
+
+
+-- SELECT * FROM ta_local_lote WHERE DT_EXCLUSAO_LOGICA IS NOT NULL 
+
+--use teras  
+
+--SELECT c.name AS ColName, t.name AS TableName
+--FROM sys.columns c
+--    JOIN sys.tables t ON c.object_id = t.object_id
+--WHERE c.name LIKE '%innerboundaryisz%'
+
+
+--select * from sendemergencycalllocation 
+
+--sendemergencycalllocation
+
+--cod_shape_type	shape_type
+--cod_shape_type	mlp_shape
+--srsname	mlp_shape
+--distanceUnit	mlp_shape
+-- angularUnit	mlp_shape
+--gid	mlp_shape
+
+--x	polygon
+--x	mlp_shape
+--y	polygon
+--y	mlp_shape
+--z	polygon
+--z	mlp_shape
+--radius	mlp_shape
+--inradius	mlp_shape
+--outradius	mlp_shape
+-- startangle	mlp_shape
+--stopangle	mlp_shape
+
+--angle	mlp_shape
+--startangle	mlp_shape
+--stopangle	mlp_shape
+
+--semiMajor	mlp_shape
+
+
+--select * from mlp_shape
+
+--srsname
+
+
+
+
+
+--use teras 
+
+--ALTER TABLE sendemergencycalllocation 
+--	ADD outerboundaryisx  varchar(200),
+--	outerboundaryisy      varchar(200),
+--	outerboundaryisz      varchar(200),
+--	innerboundaryisx      varchar(200),
+--	innerboundaryisy      varchar(200),
+--	innerboundaryisz      varchar(200)
+	 
+
+--ALTER TABLE sendsms
+--	ADD outerboundaryisx  varchar(200),
+--	outerboundaryisy      varchar(200),
+--	outerboundaryisz      varchar(200),
+--	innerboundaryisx      varchar(200),
+--	innerboundaryisy      varchar(200),
+--	innerboundaryisz      varchar(200)
+	
+
+
+-- fone dados
+
+
